f24b8d30b1f694902eaeef9af3beda4cb87618fa
zaron4k <76218026+zaron4k@users.noreply.github.com>
2024-12-17 14:07:07 +0200

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+
+            //List<string> list = new List<string>() { "Mandarin", "Apelsin", "Limon", "Pomela", "Lime", "Klementina" };
+
+            ////1)добавьте в список элементы строчными буквами;
+            //var citrus = new List<string>();
+            //foreach (var fruit in list)
+            //{
+            //    citrus.Add(fruit.ToLower());
+            //}
+
+            ////2)выведите содержимое коллекции на экран;
+            //Console.WriteLine("Содержимое коллекции (строчные буквы):");
+            //foreach (var fruit in citrus)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+            ////3)преобразуйте все буквы элементов в прописные;
+            //for (int i = 0; i < citrus.Count; i++)
+            //{
+            //    citrus[i] = citrus[i].ToUpper();
+            //}
+
+            ////4)выведите содержимое коллекции на экран;
+            //Console.WriteLine("Содержимое коллекции (прописные буквы):");
+            //foreach (var fruit in citrus)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+            ////5)определите текущее количество элементов коллекции;
+            //Console.WriteLine("Количество элементов в коллекции: " + citrus.Count);
+
+            ////6)добавьте в коллекцию новый элемент в конец списка соответствующего варианта;
+            //citrus.Add("Grapefruit");
+
+            ////7)выведите содержимое коллекции на экран;
+            //Console.WriteLine("Содержимое коллекции после добавления нового элемента:");
+            //foreach (var fruit in citrus)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+
+            //    //8)добавьте в коллекцию новый элемент в начале списка соответствующего варианта;
+            //    Console.WriteLine("Содержимое коллекции:");
+            //        foreach (var fruit in list)
+            //        {
+            //            Console.WriteLine(fruit);
+            //        }
+
+            //    //9)выведите содержимое коллекции на экран;
+            //    list.Insert(0, "Grapefruit");
+
+
+            //        Console.WriteLine("Содержимое коллекции после добавления нового элемента:");
+            //        foreach (var fruit in list)
+            //        {
+            //            Console.WriteLine(fruit);
+            //        }
+
+            //    //10)удалите третий элемент коллекции;
+            //    list.RemoveAt(2);
+
+            //    //11)выведите содержимое коллекции на экран;
+            //    Console.WriteLine("Содержимое коллекции после удаления третьего элемента:");
+            //        foreach (var fruit in list)
+            //        {
+            //            Console.WriteLine(fruit);
+            //        }
+
+            //    //12)расположите элементы в обратном порядке
+            //    list.Reverse();
+
+            //    Console.WriteLine("Содержимое коллекции после разворота:");
+            //    foreach (var fruit in list)
+            //        {
+            //            Console.WriteLine(fruit);
+            //        }
+
+
+            //    Console.ReadKey();
+
+
+
+            //Задание 2.Записана информация о стоимости каждого из 20 видов товара, продаваемых фирмой.
+            //    С 1 января очередного года фирма прекращает продавать товар, стоимость которого записана 
+            //    в n - м элементе списка. Получить список со стоимостью всех оставшихся видов товара.
+
+
+            }
+    }
+    }
